3c17683762944c83f220b24ed258859f0a37dc70
Yunkyeong <yunkyeong@ccmedia.co.kr>
2025-07-07 00:41:10 +0900

    feat: 레이어 계층별로 분리하고 패키지 생성
    controller, service 레이어로 분리하여 비즈니스 로직은 service 단에 다 넣음.
    패키지도 별도로 분리하고 domain  패키지도 생성.

---
+++ src/main/java/io/hhplus/tdd/point/controller/PointController.java
@@ -0,0 +1,63 @@
+package io.hhplus.tdd.point.controller;
+
+import io.hhplus.tdd.point.domain.PointHistory;
+import io.hhplus.tdd.point.domain.UserPoint;
+import io.hhplus.tdd.point.service.PointService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/point")
+public class PointController {
+
+    private static final Logger log = LoggerFactory.getLogger(PointController.class);
+    private final PointService pointService;
+    /**
+     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
+     */
+    @GetMapping("{id}")
+    public UserPoint point(
+            @PathVariable long id
+    ) {
+
+        UserPoint userPoint = pointService.getUserPoint(id);
+        return userPoint;
+    }
+
+    /**
+     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
+     */
+    @GetMapping("{id}/histories")
+    public List<PointHistory> history(
+            @PathVariable long id
+    ) {
+        return List.of();
+    }
+
+    /**
+     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
+     */
+    @PatchMapping("{id}/charge")
+    public UserPoint charge(
+            @PathVariable long id,
+            @RequestBody long amount
+    ) {
+        return new UserPoint(0, 0, 0);
+    }
+
+    /**
+     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
+     */
+    @PatchMapping("{id}/use")
+    public UserPoint use(
+            @PathVariable long id,
+            @RequestBody long amount
+    ) {
+        return new UserPoint(0, 0, 0);
+    }
+}
--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -1,2 +1,94 @@
-package io.hhplus.tdd.point.service;public class PointService {
+package io.hhplus.tdd.point.service;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import io.hhplus.tdd.point.domain.UserPoint;
+import io.hhplus.tdd.point.domain.constant.TransactionType;
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.domain.PointHistory;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class PointService {
+
+    private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
+
+    /**
+     * 유저 포인트 조회
+     * 
+     * 1. 유저 포인트 조회
+     * 2. 유저 포인트 반환
+     * 
+     * @param userId
+     * @return
+     */
+    public UserPoint getUserPoint(long userId) {
+        log.info("Getting user point for userId: {}", userId);
+        return userPointTable.selectById(userId);
+    }
+    
+    /**
+     * 포인트 내역 조회
+     * 
+     * 1. 유저 포인트 내역 조회
+     * 2. 유저 포인트 내역 반환
+     * 
+     * @param userId
+     * @return
+     */
+    public List<PointHistory> getPointHistory(long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
+    }
+
+    /**
+     * 포인트 충전
+     * 
+     * 1. 유저 포인트 조회
+     * 2. 유저 포인트가 0보다 작으면 예외 발생
+     * 3. 유저 포인트 업데이트
+     * 4. 포인트 내역 저장
+     * 5. 유저 포인트 반환
+     * @param userId
+     * @param amount
+     * @return
+     */
+    public UserPoint chargePoint(long userId, long amount) {
+        UserPoint userPoint = userPointTable.selectById(userId);
+        if (userPoint.point() + amount < 0) {
+            throw new IllegalArgumentException("잔고가 부족합니다");
+        }
+        userPointTable.insertOrUpdate(userId, userPoint.point() + amount);
+        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+        return userPoint;
+    }
+
+    /* 
+     * 포인트 사용
+     * 
+     * 1. 유저 포인트 조회
+     * 2. 유저 포인트가 0보다 작으면 예외 발생
+     * 3. 유저 포인트 업데이트
+     * 4. 포인트 내역 저장
+     * 5. 유저 포인트 반환
+     * 
+     * @param userId
+     * @param amount
+     * @return
+     */
+    public UserPoint usePoint(long userId, long amount) {   
+        UserPoint userPoint = userPointTable.selectById(userId);
+        if (userPoint.point() - amount < 0) {
+            throw new IllegalArgumentException("잔고가 부족합니다");
+        }
+        userPointTable.insertOrUpdate(userId, userPoint.point() - amount);
+        pointHistoryTable.insert(userId, amount, TransactionType.USE, System.currentTimeMillis());
+        return userPoint;
+    }
 }
